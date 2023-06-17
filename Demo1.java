dcad457349b5aabc92bd428c4af268eb5afd30ed
Santhoshraju-Ch <santhosh.ch192@gmail.com>
2023-06-17 14:05:10 +0530

    one more add sysout

--- Demo1.java
+++ Demo1.java
@@ -1,8 +1,9 @@
 class Demo1{
 	
 	public static void main(string args[]){
-	System.out.println("Demo");
+	System.out.println("Demo1");
+	System.out.println("Demo2");
+	System.out.println("Demo3");
 
-		System.out.println("Demo1");
-	}
+}
 }
